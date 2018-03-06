164dc66f87b6528f236617543a50be340365895a
何海水 <270232645@qq.com>
2018-03-07 00:05:00 +0800

    1.0.4

--- agentclient/src/main/java/org/hhs/App.java
+++ agentclient/src/main/java/org/hhs/App.java
@@ -2,47 +2,57 @@ package org.hhs;
 
 import lombok.extern.slf4j.Slf4j;
 import org.hhs.utils.LoadClassName;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
+import java.io.File;
 import java.io.IOException;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.net.URL;
+import java.net.URLClassLoader;
+import java.util.ArrayList;
+import java.util.Enumeration;
 import java.util.List;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
 
 /**
  * Hello world!
  *
  */
-@Slf4j
 public class App {
-    public static void main( String[] args ) throws IOException {
+    private static Logger logger = LoggerFactory.getLogger("RollingFile-normal");
+    public static void main( String[] args ) throws Exception {
+        CountDownLatch countDownLatch = new CountDownLatch(2);
         init();
-        ServerSocket socket = new ServerSocket(8001);
-        Socket socketClient = socket.accept();
+        countDownLatch.await();
     }
 
     public static void init(){
-        ScheduledExecutorService executorService = Executors.newScheduledThreadPool(4);
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
         LoadClassName loadClassName = new LoadClassName();
         List<String> stringList = loadClassName.getStringList();
-
         for (String string : stringList){
-            System.out.println(string);
             try {
                 Class clazz = Class.forName(string);
                 try {
                     Object obj = clazz.newInstance();
                     executorService.scheduleAtFixedRate((Runnable) obj, 0, 1, TimeUnit.SECONDS);
                 } catch (InstantiationException e) {
-                    log.error("class InstantiationException error:{}",string+".class", e);
+                    logger.error("class InstantiationException error:{}",string+".class", e);
                 } catch (IllegalAccessException e) {
-                    log.error("class IllegalAccessException error:{}",string+".class", e);
+                    logger.error("class IllegalAccessException error:{}",string+".class", e);
                 }
             } catch (ClassNotFoundException e) {
-                log.error("load class error:{}",string+".class", e);
+                logger.error("load class error:{}",string+".class", e);
             }
         }
     }
+
+
 }
--- agentclient/src/main/java/org/hhs/cmd/ExecShell.java
+++ agentclient/src/main/java/org/hhs/cmd/ExecShell.java
@@ -2,55 +2,62 @@ package org.hhs.cmd;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-
-import java.io.IOException;
-import java.io.InputStream;
+import java.io.*;
 
 public class ExecShell {
 
     private Logger logger = LoggerFactory.getLogger("RollingFileErr");
 
-    public  InputStream getInputStream(String cmd){
-        java.lang.Runtime runtime = Runtime.getRuntimeInstance();
-        InputStream in = null;
+    private Object exec(String[] cmdLinux){
         Process process = null;
+        LineNumberReader br = null;
+        StringBuffer sb = null;
         try {
-            process = runtime.exec(new String[] {"/bin/sh", "-c", cmd});
-            in= process.getInputStream();
-            process.destroy();
-            return in;
+            process = java.lang.Runtime.getRuntime().exec(cmdLinux);
+            br = new LineNumberReader(new InputStreamReader(process.getInputStream(), "GB18030"));
         } catch (IOException e) {
-            in = process.getErrorStream();
-            logger.error("exec shell {} error", cmd, e);
-            return in;
-        }finally {
+            logger.error("exec {} error", cmdLinux, e);
             try {
-                in.close();
+                br = new LineNumberReader(new InputStreamReader(process.getErrorStream(), "GB18030"));
+            } catch (UnsupportedEncodingException e1) {
+                e1.printStackTrace();
+            }
+        }finally {
+            sb = new StringBuffer();
+            String line;
+            try {
+                while ((line = br.readLine()) != null){
+                    sb.append(line).append("\n");
+                }
             } catch (IOException e) {
-                logger.error("inputstream close error", e);
+                logger.error("exec cmd error:readLine", e);
             }
         }
+        return sb.toString().replaceAll("%(?![0-9a-fA-F]{2})", "%25");
     }
 
-    private static class Runtime{
-        private static java.lang.Runtime runtime = null;
+    public Object execLinux(String cmd){
+        String[] cmdLinux = {"/bin/sh", "-c", cmd};
+        return exec(cmdLinux);
+    }
 
-        private Runtime(){
+    private ExecShell(){
 
-        }
+    }
 
-        public static java.lang.Runtime getRuntimeInstance(){
-            if (runtime == null){
-                synchronized (Runtime.class){
-                    if (runtime == null){
-                        runtime = java.lang.Runtime.getRuntime();
-                    }
+    private static ExecShell execShell = null;
+
+    public static ExecShell getExecShellInstance(){
+        if (execShell == null){
+            synchronized (ExecShell.class){
+                if (execShell == null){
+                    execShell = new ExecShell();
                 }
             }
-            return runtime;
         }
-
+        return execShell;
     }
 
 }
+
 
