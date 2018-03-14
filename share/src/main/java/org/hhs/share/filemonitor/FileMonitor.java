22b231e0359c346037e8400d8ba6eff786d2fdf3
何海水 <270232645@qq.com>
2018-03-14 23:32:43 +0800

    1.0.10

---
+++ share/src/main/java/org/hhs/share/filemonitor/FileMonitor.java
@@ -0,0 +1,30 @@
+package org.hhs.share.filemonitor;
+
+import org.apache.commons.io.monitor.FileAlterationListener;
+import org.apache.commons.io.monitor.FileAlterationMonitor;
+import org.apache.commons.io.monitor.FileAlterationObserver;
+
+import java.io.File;
+
+public class FileMonitor {
+    FileAlterationMonitor monitor = null;
+
+    public FileMonitor(long interval){
+        monitor = new FileAlterationMonitor(interval);
+    }
+
+    public void monitor(String path, FileAlterationListener listener){
+        FileAlterationObserver observer = new FileAlterationObserver(new File(path));
+        monitor.addObserver(observer);
+        observer.addListener(listener);
+    }
+
+    public void stop() throws Exception {
+        monitor.stop();
+    }
+
+    public void start() throws Exception {
+        monitor.start();
+    }
+
+}
